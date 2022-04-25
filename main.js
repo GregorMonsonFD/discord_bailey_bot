55e9ceee5408bccc5b1a7983024491a0715959f4
GregorMonsonFD <eggzo@DESKTOP-8SBVOPL.localdomain>
2022-04-25 20:43:24 +0100

    Added joke harassment

--- main.js
+++ main.js
@@ -2,18 +2,23 @@ require("dotenv").config();
 
 console.log('Running...\n');
 
-const {Client, Intents} = require('discord.js');
+const {Client, Intents, Message} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.login(process.env.BOTTOKEN);
 
 client.on('ready', READY_DISCORD);
-client.on('message', BORKLOL)
+client.on('messageCreate', BORKLOL)
 
 function READY_DISCORD() {
     console.log('Hooray! Run successfully');
 }
 
 function BORKLOL(msg) {
-    msg.reply('BORK!');
+    console.log(msg.content)
+    if(msg.author.id === "765509287954481152")
+    { 
+        if(msg.content === "give treat"){return msg.reply('*NOMNOMNOM*');}
+        else {return msg.reply('**BORK!**');}
+    }
 }
